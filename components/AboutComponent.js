96b7be118286d64ce52e69982d8866e8d0903958
Ash Hussain <ashguy110@gmail.com>
2022-04-16 12:41:25 -0500

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { Text, ScrollView, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -17,7 +18,6 @@ class About extends Component {
     }
 
     render() {
-
         const renderPartner = ({item}) => {
             return (
                 <ListItem
@@ -25,6 +25,29 @@ class About extends Component {
                     subtitle={item.description}
                     leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />    
+            );
+        }
+
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
             );
         }
 
